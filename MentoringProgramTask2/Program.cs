599afe01172973427d09deb459c335c3a221f3ab
MOSCOW\Yekaterina_Bulekova <Yekaterina_Bulekova@epam.com>
2018-01-24 15:13:21 +0600

    My first realeas of Task2

---
+++ MentoringProgramTask2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using StringToIntParserLibrary;
+
+namespace MentoringProgramTask2
+{
+    internal class Program
+    {
+        protected static void Main()
+        {
+
+            const int count = 5;
+            var userStrings = new string[count];
+
+
+            #region First solution of task2 part 1
+
+            Console.WriteLine($"Input {count} strings:");
+            var i = 0;
+            while (i < count)
+            {
+                Console.Write($"{i + 1} : ");
+                try
+                {
+                    var tempString = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(tempString))
+                        userStrings[i++] = tempString;
+                    else
+                        throw new ArgumentNullException($"Don't input string!");
+                }
+                catch (ArgumentNullException e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            Console.WriteLine($"First chars of string:");
+            for (var j = 0; j < count; j++)
+            {
+                Console.WriteLine($"{j + 1} : {userStrings[j][0]}");
+            }
+
+            #endregion
+
+            #region Second solution of task2 part 1
+
+            Console.WriteLine($"Input {count} strings:");
+            for (var j = 0; j < count; j++)
+            {
+                userStrings[j] = Console.ReadLine();
+            }
+
+            Console.WriteLine($"First chars of string:");
+            for (var j = 0; j < count; j++)
+            {
+                try
+                {
+                    try
+                    {
+                        Console.WriteLine($"{j + 1} : {userStrings[j][0]}");
+                    }
+                    catch (IndexOutOfRangeException e)
+                    {
+                        throw new IndexOutOfRangeException("Input empty string!", e);
+                    }
+                    catch (NullReferenceException e)
+                    {
+                        throw new NullReferenceException("Input null string!", e);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            #endregion
+
+            #region Test for task2 part 2
+
+            try
+            {
+                Console.Write($"Integer number: ");
+                var numString = Console.ReadLine();
+                Console.WriteLine($"{numString.StringToIntParse()} was input");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            #endregion
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringToIntParserLibrary/StringToIntParser.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace StringToIntParserLibrary
+{
+    /// <summary>
+    /// A class that provide parse from string to integer functionality
+    /// </summary>
+    public static class StringToIntParser
+    {
+        private static readonly Dictionary<char, int> Digits = new Dictionary<char, int>()
+        {
+            { '0', 0 },
+            { '1', 1 },
+            { '2', 2 },
+            { '3', 3 },
+            { '4', 4 },
+            { '5', 5 },
+            { '6', 6 },
+            { '7', 7 },
+            { '8', 8 },
+            { '9', 9 },
+        };
+
+        /// <summary>
+        /// Extention method for String type that convert string to integer 
+        /// </summary>
+        /// <param name="str"> A string that represents some number </param>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="FormatException"></exception>
+        /// <exception cref="OverflowException"></exception>
+        /// <returns> Integer that was represented in string </returns>
+        public static int StringToIntParse(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str)) throw new ArgumentNullException(string.Empty, $"String is empty");
+
+            var resultInt = 0;
+            var isNegative = false;
+            var rgx = new Regex(@"^-?\d+");
+
+            if (!rgx.IsMatch(str)) throw new FormatException($"Wrong format of number");
+            if (str[0] == '-')
+            {
+                str = str.Substring(1);
+                isNegative = true;
+            }
+
+            var power = str.Length - 1;
+            if (power > 9) throw new OverflowException($"Too long number");
+            foreach (var chr in str)
+            {
+                try
+                {
+                    checked
+                    {
+                        resultInt += Digits[chr] * GetMultiplier(power--);
+                    }
+                }
+                catch (OverflowException)
+                {
+                    throw new OverflowException($"Too long number");
+                }
+            }
+
+            return isNegative ? -1 * resultInt : resultInt;
+        }
+
+        private static int GetMultiplier(int power)
+        {
+            var multipier = 1;
+            for (var j = 0; j < power; j++)
+            {
+                multipier *= 10;
+            }
+
+            return multipier;
+        }
+    }
+}
